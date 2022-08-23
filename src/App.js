0a9e092fc88758f777e9be9ce31f0c728ec79051
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2022-08-22 17:08:48 -0700

    webpack added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        hello world
+      </div>
+    </React.StrictMode>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+};
