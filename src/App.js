b852c00cead69feb585783e03f1c3223583bbca2
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2022-08-24 15:54:01 -0700

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Styles from "./app.module.css";
 import { Footer } from "./Footer";
+import { Header } from "./Header";
 
 const App = () => {
   return (
     <React.StrictMode>
+      <Header />
       <div className={Styles.container}>hello fresh</div>
       <Footer />
     </React.StrictMode>
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div>
-      Footer 1<div>{process.env.CHANGE_ME}</div>
+      Footer env {process.env.CHANGE_ME}
     </div>
   );
 };
