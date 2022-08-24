5781f2bb98a4878702e8c53b573d67de9ff38a3d
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2022-08-24 16:28:39 -0700

    styles added

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Styles from '../global-styles/global.css';
 
 export const Footer = () => {
   return (
-    <div>
-      Footer env {process.env.CHANGE_ME}
+    <div className={Styles["container"]}>
+      Footer envs {process.env.CHANGE_ME}
     </div>
   );
 };
